daac0a6510bbc32ddc9336a1e9a73df3e5e78758
Akshay Patel <akshaypatel07@hotmail.com>
2021-02-22 12:21:59 -0500

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::cout << "This Chess game was created using C++ for a final project for the CS 246 Winter 2019 course offering at the Universty of Waterloo." << std::endl;
+	std::cout << "This project was started at the end of March 2019 and finished in early April 2019." << std::endl;
+	std::cout << "The goal of this project was to use OOP principles and design patterns learned in class to develop a large project." << std::endl << std::endl;
+
+	std::cout << "Please contact me at akshay.patel@uwaterloo.ca if you would like to see the source code." << std::endl;
+	std::cout << "Since CS 246 is still being offered at the University of Waterloo, I cannot make the source code public due to academic policy." << std::endl;
+}
