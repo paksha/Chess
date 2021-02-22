0b89042b9f74cb3a5716491018587e8ef4cbf70e
Akshay Patel <akshaypatel07@hotmail.com>
2021-02-22 12:19:50 -0500

    Delete main.cc

--- main.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "Please contact me at akshay.patel@uwaterloo.ca if you would like to see the source code." << std::endl;
-	std::cout << "Since CS 246 is still being offered at the University of Waterloo, I cannot make the source code public due to academic policy." << std::endl;
-}
